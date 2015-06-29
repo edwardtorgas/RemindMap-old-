2cb3843c42da256ba105d4b3d82c8fc6c636c2d2
Judy Lin <julin@hmc.edu>
2015-06-29 15:54:00 -0700

    Criminal Intent Template

---
+++ app/src/main/java/com/fb/android/remindmap/database/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.fb.android.remindmap.database;
+
+/**
+ * Created by judyl on 6/22/15.
+ */
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+            public static final String SUSPECT = "suspect";
+        }
+    }
+}
